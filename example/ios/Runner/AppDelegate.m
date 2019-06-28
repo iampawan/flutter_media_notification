f4e837d313db38ecf7bd264e2f64063239e6a9d4
Michael Cadigan <michael@numo.com>
2019-06-28 18:56:51 -0400

    no message

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,5 +1,7 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @implementation AppDelegate
 
@@ -7,7 +9,32 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    NSError *error = nil;
+    if (![[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:&error]) {
+        NSLog(@"%@", error);
+    }
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+
 }
+
+
+//- (void)remoteControlReceivedWithEvent:(UIEvent *)receivedEvent {
+//    if (receivedEvent.type == UIEventTypeRemoteControl) {
+//        switch (receivedEvent.subtype) {
+//            case UIEventSubtypeRemoteControlPlay:
+//                [[NSNotificationCenter defaultCenter] postNotificationName:kCYCAppDelegatePlayPauseNotificationName object:nil];
+//                break;
+//            case UIEventSubtypeRemoteControlPause:
+//                [[NSNotificationCenter defaultCenter] postNotificationName:kCYCAppDelegatePlayPauseNotificationName object:nil];
+//                break;
+//            case UIEventSubtypeRemoteControlTogglePlayPause:
+//                [[NSNotificationCenter defaultCenter] postNotificationName:kCYCAppDelegatePlayPauseNotificationName object:nil];
+//                break;
+//            default:
+//                break;
+//        }
+//    }
+//}
 
 @end
--- ios/Classes/MediaNotificationPlugin.m
+++ ios/Classes/MediaNotificationPlugin.m
@@ -1,4 +1,7 @@
 #import "MediaNotificationPlugin.h"
+#import <AVFoundation/AVPlayer.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
 
 @implementation MediaNotificationPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -10,10 +13,29 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
+  if ([@"show" isEqualToString:call.method]) {
+      NSString* title = call.arguments[@"title"];
+      NSString* author = call.arguments[@"author"];
+      BOOL play = call.arguments[@"play"];
+    
+      NSMutableDictionary *albumInfo = [[NSMutableDictionary alloc] init];
+     // UIImage *artWork = [UIImage imageNamed:album.imageUrl];
+      [albumInfo setObject:title forKey:MPMediaItemPropertyTitle];
+      [albumInfo setObject:author forKey:MPMediaItemPropertyArtist];
+     // [albumInfo setObject:album.title forKey:MPMediaItemPropertyAlbumTitle];
+     // [albumInfo setObject:artworkP forKey:MPMediaItemPropertyArtwork];
+     // [albumInfo setObject:album.playrDur forKey:MPMediaItemPropertyPlaybackDuration]
+    //  [albumInfo setObject:album.elapsedTime forKey:MPNowPlayingInfoPropertyElapsedPlaybackTime]
+      [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:albumInfo];
+     
+      if (play == YES) {
+          [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+          [[AVAudioSession sharedInstance]setActive:YES error:nil];
+      }
+      
+      //setup and receive remote control events:
+
+      result(@"success");
   }
 }
 
